b8b4938e766498420fc017e21a0a00a4fcbdadda
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-03-17 14:35:27 +0530

    update Pay

--- frontend/src/components/Pay.tsx
+++ frontend/src/components/Pay.tsx
@@ -1,65 +1,27 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import '../css/Pay.css';
-import { AmountState } from '../states/AmountState';
-import { PayState } from '../states/PayState';
-import { SuccessState } from '../states/SuccessState';
-import Navbar from './Navbar';
+import Navbar from './Navbar'
+import '../css/Pay.css'
+import { useSetRecoilState } from 'recoil'
+import { AmountState } from '../states/AmountState'
+import {useNavigate} from 'react-router-dom'
 
-export default function Pin() {
+export default function Pay() {
     const navigate = useNavigate();
-    const [pin, setPin] = useState('');
-    const receiver = useRecoilValue(PayState);
-    const amount = useRecoilValue(AmountState);
-    const setSuccess = useSetRecoilState(SuccessState);
-    const [loading, setLoading] = useState(false)
-    
-    const handlePay = async(e: React.MouseEvent) => {
-        e.preventDefault();
-        setLoading(true);
-        try {
-            const response = await fetch(`https://backend.nithin-kanduru1908.workers.dev/api/transactions/makePayment`, {
-              method: "PUT",
-              headers: {
-                  "Content-Type": "application/json",
-                  "auth-token": localStorage.getItem("token") || ""
-                },
-              body: JSON.stringify({
-                "receiverUserId":`${receiver}`,
-                "tsxnAmount": amount
-              }),
-      });
-            const json = await response.json();
-            setLoading(false)
-            if(!json.success){
-                navigate('/success');
-            }
-            else{
-                setSuccess(true);
-                navigate('/success');
-            }
-        } catch (error) {
-            setSuccess(false)
-            navigate('/success');
-        }
-    }
+    const setAmount = useSetRecoilState(AmountState);
   return (
     <div className='pay-wrapper'>
         <Navbar/>
-        {!loading && <div className="pay display-flex align-center justify-center">
+        <div className="pay display-flex align-center justify-center">
             <div className="pay-block display-flex justify-center">
                 <div className="pay-container display-flex flex-column gap-1">
-                <input type="text" name="amount" className='tsxnamount' maxLength={4} minLength={4} value={pin} onChange={(e) => {
+                <input type="text" name="amount" className='tsxnamount' maxLength={7} onChange={(e : any) => {
                     e.preventDefault();
-                    setPin(e.target.value);
-                }} placeholder='Pin' id="tsxnPin" />
-                <button className='pay-pay-button' onClick={handlePay}>Pay</button>
+                    setAmount(e.target.value);
+                }}  placeholder='Amount' id="tsxnAmount" />
+                <button className='pay-pay-button' onClick={() => {
+                    navigate('/pin')
+                }}>Pay</button>
                 </div>
             </div>
-        </div>}
-        <div className="display-flex align-center-justify-center">
-           {loading && <span>Please wait...</span>}
         </div>
     </div>
   )
